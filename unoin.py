1e3e9f832d16502041c5012f9efcd3f78affc7a0
AlinaAlinaAlinaa <greenlighttt.97@gmail.com>
2026-01-19 17:29:41 +0600

    Поменяла на list

--- unoin.py
+++ unoin.py
@@ -4,6 +4,6 @@ with open("file1.txt") as f1:
 with open("file2.txt") as f2:
     element2 = [int(numbers) for numbers in f2]
     
-duplicates={numbers for numbers in element1  if numbers in element2}
+duplicates=[numbers for numbers in element1  if numbers in element2]
 print(duplicates)
 
